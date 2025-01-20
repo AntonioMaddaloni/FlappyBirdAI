2144859ba50150c04163f8983574d9bf1a350d9a
Francesco Peluso <francesco.peluso@agtweb.it>
2025-01-20 15:44:04 +0100

    def_dqn

--- DQN/__main__.py
+++ DQN/__main__.py
@@ -1,18 +1,18 @@
 import gymnasium as gym
-from stable_baselines3 import PPO
+from stable_baselines3 import DQN
 
 # Stampa tutti gli ambienti registrati
 print("Ambienti registrati:", gym.envs.registry.keys())
 
 # Assicurati che l'ambiente sia registrato
-env = gym.make('FlappyBird-v0',render_mode = "human")
+env = gym.make('FlappyBird-v0')
 
 # Configura e addestra il modello PPO
-model = PPO("MlpPolicy", env, verbose=1, batch_size=256,n_steps=32768)
+model = DQN("MlpPolicy", env, verbose=1, batch_size=256,buffer_size=32768)
 # Carica il modello salvato
 #model = PPO.load("./Models/ppo_flappybird", env=env)
 #addestramento
 model.learn(total_timesteps=1000000)
 
 # Salva il modello
-#model.save("./Models/ppo_flappybird")
+model.save("./Models/dqn_flappybird")
